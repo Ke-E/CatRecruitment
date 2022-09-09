597101cdf685b8d35081a573147278783efd7c59
k.enomoto <red.ke.5084@gmail.com>
2022-09-09 09:55:14 +0900

    ・ロガーの設定をjsonの設定ファイル依存でなく、クラス内にて定義するよう修正（ログファイル名に日付を出力するため） ・ロガー用の設定ファイル(json)を削除

--- src/main/logger/logger.py
+++ src/main/logger/logger.py
@@ -1,6 +1,8 @@
 import sys
 import json
+from datetime import datetime
 from logging import getLogger, config as logging_config
+import logging.handlers
 sys.path.append('../')
 from propeties import config
 
@@ -8,15 +10,30 @@ class Logger:
     """Loggerを生成する操作を行う
     """
 
+    # ログレベルを引数に設定し、予備元が好きなレベルのロガーを生成できるようにする
     def get_logger(self):
-        """loggerを生成、返す
+        """ログレベルがINFOなloggerを生成、返す
 
         Returns:
             Logger: 汎用的な出力を行うlogger
 
         """
-        with open(config.ROOT_PATH + config.LOG_CONFIG_PATH, 'r') as f:
-            log_conf = json.load(f)
+        # loggerの生成
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
 
-        logging_config.dictConfig(log_conf)
-        return getLogger('main')
+        # ログファイルハンドラーの設定
+        log_name = config.LOG_PATH + config.LOG_NAME
+        fh = logging.FileHandler(log_name.format(datetime.now()))
+        fh.setLevel(logging.INFO)
+        fh_formatter = logging.Formatter(fmt=config.LOG_OUTPUT_FMT, datefmt=config.LOG_DATE_FMT)
+        fh.setFormatter(fh_formatter)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        ch_formatter = logging.Formatter(fmt=config.LOG_OUTPUT_FMT, datefmt=config.LOG_DATE_FMT)
+        ch.setFormatter(ch_formatter)
+
+        logger.addHandler(fh)
+        logger.addHandler(ch)
+        return logger
